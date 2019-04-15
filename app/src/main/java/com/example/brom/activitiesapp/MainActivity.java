63c48cae4740cb6b09318cff40285c834572d8e8
a18eliek <a18eliek@student.his.se>
2019-04-15 11:34:40 +0200

    Lade in ListView-objekt som skickar data med hjälp utav intent till klassen MountainDetailsActivity

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -16,15 +16,48 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    // Create ArrayLists from the raw data above and use these lists when populating your ListView.
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
     private int[] mountainHeights ={4478,4808,6190};
-    // Create ArrayLists from the raw data above and use these lists when populating your ListView.
 
+    public static final String MOUNTAIN_NAME = "MOUNTAIN_NAME", MOUNTAIN_LOCATION = "MOUNTAIN_LOCATION", MOUNTAIN_HEIGHT = "MOUNTAIN_HEIGHT", MOUNTAIN_ID = "MOUNTAIN_ID";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("brom", "onCreate() called.");
+
+
+        //Skapar ett List objekt med alla berg. Se array mountainNames.
+        List<String> listData = new ArrayList<String>(Arrays.asList(mountainNames));
+
+        //Skapar en ArrayAdapter som ansluter till list_item_textview, my_item_textview samt med List objektet.
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.list_item_textview, R.id.my_item_textview, listData);
+
+        //Hittar layout elementet my_listview och skapar en objekt instans.
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+
+        //Ansluter ArrayAdaptern med ListView objektet.
+        myListView.setAdapter(adapter);
+
+        //Skicka all information vid klick till vår intent
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent myIntent = new Intent(view.getContext(), MountainDetailsActivity.class);
+                myIntent.putExtra(MOUNTAIN_NAME, mountainNames[position]);
+                myIntent.putExtra(MOUNTAIN_LOCATION, mountainLocations[position]);
+                myIntent.putExtra(MOUNTAIN_HEIGHT, Integer.toString(mountainHeights[position]));
+                myIntent.putExtra(MOUNTAIN_ID, Integer.toString(position));
+
+                startActivity(myIntent);
+            }
+        });
+
+    }
 
     @Override
     protected void onStop() {
